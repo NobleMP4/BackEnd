80ab04d8b0cf2def076a9f0e94fb45f04520c3c9
NobleMP4 <165203343+NobleMP4@users.noreply.github.com>
2026-03-26 20:42:14 +0100

    aaaa

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  // Crée l'application NestJS
   const app = await NestFactory.create(AppModule);
   
-  // AJOUTE CETTE LIGNE ICI
-  app.enableCors(); 
-  
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
-}
-bootstrap();
+  // Active CORS pour autoriser ton front local et ton front sur Railway
+  app.enableCors({
+    origin: [
+      "http://localhost:5173", // front en local
+      "https://policeacademy-mr-production-256b.up.railway.app" // front en prod
+    ]
+  });
 
-//Comment
+  // Applique les pipes de validation globalement
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Port dynamique fourni par Railway, fallback à 3000 pour dev local
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
